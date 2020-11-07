5003b106d38e4f82f8c85531e0ec6328993b750f
Adelina Dzhaleova <dzhalelova@yandex.ru>
2020-11-07 12:23:15 +0300

    last update

--- srcs/main.c
+++ srcs/main.c
@@ -6,5 +6,32 @@
 
 int main (int argc, char **argv)
 {
+    if (argv != 2)
+    {
+        printf("Invalid argument, please write one file name of a document to check\n");
+        exit(0);
+    }
+    FILE *fd = fopen(argv[1], "r");
+    if (!fd)
+    {
+        printf("Can't open file, please check validity\n");
+        perror(errno);
+        exit(0);
+    }
+    char *document = NULL;
+    int len = fscanf("%s", &document);
+    //TODO: check the limit of the size for str, len == EOF
+    if (len != EOF)
+    {
+        printf("Reduce the size of the file or connect developers\n");
+        fclose(fd);
+        exit(1);
+    }
+    FILE *ph = fopen("phrases.txt", "r");
+    while (len )
+    {
+        
+    }
+    fclose(fd);
     return (0);
 }
